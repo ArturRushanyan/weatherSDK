870955f4fb66378eaf3c545848737d0444c78ed9
Artur <artur.rush97@gmail.com>
2024-03-09 17:49:16 +0400

    Renamed WeatherInfo to WeatherInfoSDK Add data conversion to target Json format

---
+++ WeatherSDK/WeatherSDK/Data/WeatherData.cs
@@ -0,0 +1,37 @@
+﻿namespace WeatherSDK.Data
+{
+    public class WeatherData
+    {
+        public WeatherInfo weather;
+        public TemperatureInfo temperature;
+        public int visibility;
+        public WindInfo wind;
+        public long datetime;
+        public SysInfo sys;
+        public int timezone;
+        public string name;
+    }
+
+    public class WeatherInfo
+    {
+        public string main;
+        public string description;
+    }
+
+    public class TemperatureInfo
+    {
+        public double temp;
+        public double feels_like;
+    }
+
+    public class WindInfo
+    {
+        public double speed;
+    }
+
+    public class SysInfo
+    {
+        public long sunrise;
+        public long sunset;
+    }
+}
