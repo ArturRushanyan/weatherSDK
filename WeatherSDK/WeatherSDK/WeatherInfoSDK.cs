3612eb9f158bd68bd6c0887243f5eedc0c45018d
Artur <artur.rush97@gmail.com>
2024-03-09 19:12:19 +0400

    Add readme location configs

--- WeatherSDK/WeatherSDK/WeatherInfoSDK.cs
+++ WeatherSDK/WeatherSDK/WeatherInfoSDK.cs
@@ -17,7 +17,7 @@ namespace WeatherSDK
         private static Dictionary<string, WeatherInfoSDK> instances = new Dictionary<string, WeatherInfoSDK>();
 
         private readonly int cacheSize = 10;
-        private readonly int cacheExpirationTimeInSeconds = 600;
+        private readonly int cacheExpirationTimeInSeconds = 6;
         private readonly HttpClient httpClient;
         private readonly string apiUrl = "https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}";
         private string apiKey;
