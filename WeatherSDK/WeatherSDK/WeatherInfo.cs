ab0f00f103d8b001d28af8f1a70186784f02b483
Artur <artur.rush97@gmail.com>
2024-03-09 17:22:58 +0400

    Initial commit
    Add .gitignore
    Add WeatherInfo class

---
+++ WeatherSDK/WeatherSDK/WeatherInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+
+namespace WeatherSDK
+{
+    public class WeatherInfo
+    {
+        public enum SDKMode
+        {
+            Polling,
+            OnDemend
+        }
+
+        private readonly int cacheSize = 10;
+        private readonly int cacheExpirationTimeInSeconds = 600;
+        private readonly HttpClient httpClient;
+        private readonly string apiUrl = "https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}";
+        private string apiKey;
+        private SDKMode mode;
+
+        public SDKMode Mode => mode;
+
+        private WeatherInfo(string apiKey, SDKMode mode = SDKMode.OnDemend)
+        {
+            httpClient = new HttpClient();
+            this.apiKey = apiKey;
+            this.mode = mode;
+        }
+    }
+}
